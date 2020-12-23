757eff9bb7f8f68a98c9ccadcf5910c6f0f19de4
fthompson24 <fraserjthompson@gmail.com>
2020-12-22 23:04:40 -0600

    mulitple rounds complete. Game over message

--- game.js
+++ game.js
@@ -73,7 +73,8 @@ var fight = function(enemyName) {
 
       // leave while() loop since enemy is dead
       break;
-    } else {
+    } 
+    else {
       window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
     }
 
@@ -88,14 +89,22 @@ var fight = function(enemyName) {
       window.alert(playerName + ' has died!');
       // leave while() loop if player is dead
       break;
-    } else {
+    } 
+    else {
       window.alert(playerName + ' still has ' + playerHealth + ' health left.');
     }
   }
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth = 50;
-  fight(pickedEnemyName);
+  if (playerHealth > 0) {
+    window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
+  }
+  if (playerHealth <= 0) {
+    window.alert("You have lost your robot in battle! Game OVER!");
+    break;
+  }
 };
