b4959571c0669ac073fd57bd827e88886d90a08a
fthompson24 <fraserjthompson@gmail.com>
2021-01-08 21:18:27 -0600

    fixed 'null' name bug

--- game.js
+++ game.js
@@ -156,28 +156,12 @@ var shop = function() {
   switch (shopOptionPrompt) {
     case "REFILL":
     case "refill":
-      if (playerInfo.money >= 7) {
-        window.alert("Refilling player's health by 20 for 7 dollars.");
-        //increase health and decrease money
-        playerInfo.health = playerInfo.health + 20;
-        playerInfo.money = playerInfo.money - 7;
-      }
-      else {
-        window.alert ("You don't have enough money!");
-      }
+      playerInfo.refillHealth();
       break;
 
     case "UPGRADE":
     case "upgrade":
-      if (playerInfo.money >= 7) {
-        window.alert("Upgrading player's attack by 8 for 7 dollars.");
-        //increase attack and decrease money
-        playerInfo.attack = playerInfo.attack + 8;
-        playerInfo.money = playerInfo.money - 7;
-      }
-      else {
-        window.alert ("You don't have enough money!");
-      }
+      playerInfo.upgradeAttack();
       break;
 
     case "LEAVE":
@@ -194,6 +178,16 @@ var shop = function() {
   }
 };
 
+//function to set name
+var getPlayerName = function () {
+  var name = "";
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name;
+}
+
 var randomNumber = function(min, max) {
   var value = Math.floor(Math.random() * (max - min + 1) + min);
 
@@ -201,7 +195,7 @@ var randomNumber = function(min, max) {
 };
 // Global Variables
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -209,7 +203,28 @@ var playerInfo = {
     this.health = 100;
     this.money = 10;
     this.attack = 10;
-  }
+  },
+  refillHealth: function() {
+    if (this.money >= 7) {
+      window.alert("Refilling player's health by 20 for 7 dollars.");
+    
+      this.health += 20;
+      this.money -= 7;
+    }
+    else {
+      window.alert("You don't have enough money!");
+    }
+  },
+  upgradeAttack: function() {
+    if (this.money >= 7) {
+      window.alert("Upgrading player's attack by 8 for 7 dollars.");
+      this.Attack += 8;
+      this.money -= 7;
+    }
+    else {
+      window.alert("You don't have enough money!")
+    }
+  },
 };
 
 var enemyInfo = [
